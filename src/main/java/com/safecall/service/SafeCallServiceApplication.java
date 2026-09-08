df4df911d666835a7bb6a04a149a5fbb8e03fdd9
tastysoapwater <gnslwkd13@gachon.ac.kr>
2026-09-08 15:29:35 +0900

    feat: Spring Boot 애플리케이션 기본 구조 생성
    - Spring Boot 애플리케이션 진입점 추가
    - 애플리케이션 컨텍스트 로딩 테스트 추가

---
+++ src/main/java/com/safecall/service/SafeCallServiceApplication.java
@@ -0,0 +1,12 @@
+package com.safecall.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCallServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCallServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/safecall/service/SafeCallServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.safecall.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeCallServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
